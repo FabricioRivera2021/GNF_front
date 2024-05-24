dbb90b86adc5b6f11bc0231d2a076a8ab4a81a7a
fabricio <fabricio.rivera2012@gmail.com>
2024-05-23 23:14:43 -0300

    small adds

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -44,7 +44,7 @@ export default function DefaultLayout() {
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <p className='text-white'><BeakerIcon width='30px' /></p>
+                      <h3 className='text-slate-50 font-sans text-xl border-2 pr-6 pl-1 font-semibold shadow-lg rounded-md'>logo Farmacia</h3>
                     </div>
                     {/* Botos del nav bar------------------------------------------------------------------------------- */}
                     <div className="hidden md:block">
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -6,20 +6,17 @@ export default function Login() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              className="mx-auto w-52"
+              src="./img/logo.png"
               alt="Your Company"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
           </div>
   
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
+                  Email
                 </label>
                 <div className="mt-2">
                   <input
@@ -36,11 +33,11 @@ export default function Login() {
               <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
+                    Contraseña
                   </label>
                   <div className="text-sm">
                     <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
+                      Olvidaste tu contraseña?
                     </a>
                   </div>
                 </div>
@@ -61,17 +58,10 @@ export default function Login() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Ingresar
                 </button>
               </div>
             </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
           </div>
         </div>
       </>
