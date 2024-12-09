92934b6efadd64b43a01b6872f35ddd2d382df93
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2024-12-09 18:55:24 -0300

    continuando con admin de puestos

--- src/views/Supervisor.jsx
+++ src/views/Supervisor.jsx
@@ -7,6 +7,7 @@ export const Supervisor = () => {
     const [message, setMessage] = useState(null);
     const [puestos, setPuestos] = useState([]);
     const [users, setUsers] = useState([]);
+    const [isProcesingNumber, setIsProcesingNumber] = useState([]);
 
     const getUsers = () => {
         axios
@@ -14,6 +15,15 @@ export const Supervisor = () => {
         .then(({data}) => {
             setUsers(data.users);
             console.log(data.users);
+        })
+    }
+
+    const getUserIsProcesingNumber = () => {//saber si el usuario esta atendiendo a alguien o no
+        axios
+        .get("http://localhost:8000/api/isProcesingNumber")
+        .then(({data}) => {
+            setIsProcesingNumber(data);
+            console.log(data);
         })
     }
 
@@ -63,6 +73,7 @@ export const Supervisor = () => {
     useEffect(() => {
       getAllPuestos()
       getUsers()
+      getUserIsProcesingNumber()
     }, [])
     
 
@@ -73,7 +84,7 @@ export const Supervisor = () => {
                 <h3 className='text-center text-2xl pb-5'>Menu Posiciones</h3>
                 <div className='flex flex-col gap-4 items-start'>
                     <h2 className='text-center text-xl pb-5'>Administrar Posiciones</h2>
-                    <table className="table-auto w-full">
+                    <table className="table-fixed w-full">
                         <thead className='mb-4'>
                             <tr className='text-left bg-blue-400 text-white'>
                                 <th>Posicion</th>
@@ -89,15 +100,21 @@ export const Supervisor = () => {
                         <tbody>
                                 {puestos.map((puesto) => {
                                     const assignedUser = users.find(user => user.positions_id === puesto.id);
+                                    
+                                    //si el numero tiene un usuario asignado eso quiere decir que esta siendo atendido
+                                            // Check if a processing number exists for the assigned user
+                                    const procesingNumber = assignedUser
+                                    ? isProcesingNumber.find(number => number.user_id === assignedUser.id)
+                                    : null;
 
                                     return(
                                     <tr key={puesto.id} className={`rounded-sm py-1 text-left pl-1 capitalize font-roboto text-sm odd:bg-slate-50 even:bg-gray-100`}>
                                         <td className='py-1'>{puesto.position}</td>
                                         <td>{puesto.occupied ? 'Ocupado' : 'Libre'}</td>
-                                        <td>ATENCION EN PROCESO</td>
+                                        <td>{procesingNumber ? 'En atención' : 'Sin número'}</td>
                                         <td>10:15</td>
                                         <td>{assignedUser ? assignedUser.name : '-'}</td>
-                                        <td><button className='bg-slate-500 hover:bg-blue-400 text-white px-4 shadow-md rounded-sm'>Editar</button></td>
+                                        <td><button className='bg-slate-500 hover:bg-blue-400 text-white px-4 shadow-md rounded-sm'>Editar puesto</button></td>
                                         <td><button className='bg-slate-500 hover:bg-blue-400 text-white px-4 shadow-md rounded-sm'>Liberar</button></td>
                                         <td className='flex py-1 items-center gap-2'>{puesto.active ? 'Activo' : 'Anulado'} <div className='w-4 h-4 bg-green-400'></div></td>
                                     </tr>
@@ -110,49 +127,7 @@ export const Supervisor = () => {
                         <button onClick={handleClickClearPositions} className='bg-slate-500 hover:bg-slate-600 text-white px-3 py-1 text-sm'>Limpiar posiciones</button>
                     </div>
                 </div>
-                <hr />
-                <h2 className='text-xl pb-1'>Posiciones actualmente asignadas</h2>
-                <table className="table-auto">
-                    <thead>
-                        <tr className='text-left bg-blue-400 text-white'>
-                            <th>Posicion</th>
-                            <th>Ocupada</th>
-                            <th>Usuario</th>
-                            <th>Tiempo</th>
-                            <th>Liberar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Ventanilla 1</td>
-                            <td>SI</td>
-                            <td>Usuario Nombre</td>
-                            <td>12:32</td>
-                            <td><button className='bg-slate-500 hover:bg-slate-600 text-white px-4 shadow-md rounded-sm'>Liberar</button></td>
-                        </tr>
-                        <tr>
-                            <td>Ventanilla 2</td>
-                            <td>SI</td>
-                            <td>Usuario Nombre</td>
-                            <td>12:32</td>
-                            <td><button className='bg-slate-500 hover:bg-slate-600 text-white px-4 shadow-md rounded-sm'>Liberar</button></td>
-                        </tr>
-                        <tr>
-                            <td>Caja 1</td>
-                            <td>SI</td>
-                            <td>Usuario Nombre</td>
-                            <td>12:32</td>
-                            <td><button className='bg-slate-500 hover:bg-slate-600 text-white px-4 shadow-md rounded-sm'>Liberar</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div className='w-full flex justify-start'>
-                    <div>
-                        <button onClick={handleClickClearPositions} className='bg-slate-500 hover:bg-slate-600 text-white px-3 py-1 text-sm'>Limpiar posiciones</button>
-                    </div>
-                </div>
             </div>
-
             {message ? <Message className="translate-x-0" message={message} /> : <Message className="translate-x-100" message={message} />}
             </div>
         </>
