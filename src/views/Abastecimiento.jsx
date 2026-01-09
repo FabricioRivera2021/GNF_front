812be61336f9f8430121080fb64b06c890ff53b1
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2026-01-09 15:16:15 -0300

    añadiendo pantalllas de abastecimiento

--- src/views/Abastecimiento.jsx
+++ src/views/Abastecimiento.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import AbastecimientoSideBar from '../components/AbastecimientoSideBar'
 
 export const Abastecimiento = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-full h-[100vh]'>
-      <h1 className='text-3xl font-bold text-slate-600'>Abastecimiento</h1>
-      <p className='text-orange-500 text-xl'>Esta funcionalidad no esta disponible en este momento.</p>
-    </div>
+        <div className="flex justify-between items-start">
+            <AbastecimientoSideBar />
+        </div>
   )
 }
